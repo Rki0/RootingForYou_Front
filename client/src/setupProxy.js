7ecf1f77911897c875ecd0b6be8fe95f6add6670
Rki0 <pky00823@gmail.com>
2022-07-17 14:19:30 +0900

    proxy setting to AWS EC2

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,8 +6,15 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
       // 설정된 서버 url 입력
+      // 로컬 서버(http) - 로컬 클라이언트(http) : 정상 작동, DB 데이터 정상 작동
       // target: "http://localhost:8000",
-      target: "http://13.209.66.142:8000",
+      // AWS EC2 서버(http) - 로컬 클라이언트(http) : 정상 작동, DB 데이터 정상 작동
+      target: "http://54.180.99.254:8000",
+      // 내가 해내야 하는 것은?
+      // AWS EC2 서버(http) - Netlify 클라이언트(https)
+      // 정상 작동 + DB 데이터 정상 작동이 목표
+      // 현재 Netlify 배포 완료. index.js 잘 보임. 404 오류 발생 중...
+      // 현재 AWS EC2 서버 배포 완료. index.js 잘 작동. DB 연결 잘됨.
       changeOrigin: true,
     })
   );
